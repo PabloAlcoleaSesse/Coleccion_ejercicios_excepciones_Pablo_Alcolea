8912069f0c643b8d8c47ef70b0d89761d8a995bb
Pablo Alcolea Sesse <128633352+PabloAlcoleaSesse@users.noreply.github.com>
2024-11-04 10:54:08 +0100

    Añadir comentarios al ejercio 5

--- Ej_5/main.cpp
+++ Ej_5/main.cpp
@@ -1,9 +1,10 @@
 //
 // Created by Pablo Alcolea Sesse on 2/11/24.
 //
-#include<iostream>
-#include<stdexcept>
+#include <iostream>
+#include <stdexcept>
 
+// Función que lanza una excepción de tipo runtime_error
 void lanzaExcepcion() {
     throw std::runtime_error("Error en la función lanzaExcepcion");
 }
@@ -11,14 +12,17 @@ void lanzaExcepcion() {
 int main() {
     try {
         try {
+            // Llama a la función que lanza la excepción
             lanzaExcepcion();
         }
         catch (const std::runtime_error& e) {
+            // Captura la excepción y muestra un mensaje
             std::cout << "Excepción capturada y manejada. Reactivando..." << std::endl;
             throw; // Relanza la excepción
         }
     }
     catch (const std::runtime_error& e) {
+        // Captura la excepción reactivada y muestra un mensaje
         std::cout << "Excepción reactivada capturada: " << e.what() << std::endl;
     }
 
