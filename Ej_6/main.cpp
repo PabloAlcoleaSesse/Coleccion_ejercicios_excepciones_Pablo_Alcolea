6bf8e2dbf43dd2cb495a0ce0bf8d434b309540dc
Pablo Alcolea Sesse <128633352+PabloAlcoleaSesse@users.noreply.github.com>
2024-11-04 10:54:45 +0100

    Añadir comentarios al ejercio 6

--- Ej_6/main.cpp
+++ Ej_6/main.cpp
@@ -1,14 +1,16 @@
 //
 // Created by Pablo Alcolea Sesse on 2/11/24.
 //
-#include<iostream>
-#include<stdexcept>
+#include <iostream>
+#include <stdexcept>
 
+// Función que lanza una excepción de tipo runtime_error
 void lanzaExcepcion() {
     throw std::runtime_error("Error en la función lanzaExcepcion");
 }
 
 int main() {
+    // Llama a la función que lanza la excepción
     lanzaExcepcion();
 
     return 0;
