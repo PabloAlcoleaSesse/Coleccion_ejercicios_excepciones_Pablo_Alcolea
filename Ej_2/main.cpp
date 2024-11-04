a80cef2db02eb1173a8d0492715928b422cd6674
Pablo Alcolea Sesse <128633352+PabloAlcoleaSesse@users.noreply.github.com>
2024-11-04 10:50:46 +0100

    Añadir comentarios al ejercio 2

--- Ej_2/main.cpp
+++ Ej_2/main.cpp
@@ -1,8 +1,9 @@
-//
+
 // Created by Pablo Alcolea Sesse on 2/11/24.
 //
-#include<iostream>
-#include<stdexcept>
+#include <iostream>
+#include <stdexcept>
+using namespace std;
 
 // Versión de la función que devuelve un valor de error
 int divideError(int a, int b) {
@@ -10,17 +11,17 @@ int divideError(int a, int b) {
         return -1; // valor de error
     }
     else {
-        return a / b;
+        return a / b; // realiza la división
     }
 }
 
 // Versión de la función que lanza una excepción
 int divideException(int a, int b) {
     if (b == 0) {
-        throw std::invalid_argument("División por cero"); // lanza una excepción
+        throw invalid_argument("División por cero"); // lanza una excepción
     }
     else {
-        return a / b;
+        return a / b; // realiza la división
     }
 }
 
@@ -28,7 +29,7 @@ int main() {
     // Uso de la versión que devuelve un valor de error
     int resultError = divideError(5, 0);
     if (resultError == -1) {
-        std::cout << "Error: división por cero." << std::endl;
+        cout << "Error: división por cero." << endl; // maneja el error
     }
 
     // Uso de la versión que lanza una excepción
@@ -36,7 +37,7 @@ int main() {
         int resultException = divideException(5, 0);
     }
     catch (const std::invalid_argument& e) {
-        std::cout << "Excepción: " << e.what() << std::endl;
+        cout << "Excepción: " << e.what() << endl; // maneja la excepción
     }
 
     return 0;
