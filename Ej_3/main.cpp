f97259b2530cad43e271e419557a71e748d0346b
Pablo Alcolea Sesse <128633352+PabloAlcoleaSesse@users.noreply.github.com>
2024-11-04 10:50:55 +0100

    Añadir comentarios al ejercio 3

--- Ej_3/main.cpp
+++ Ej_3/main.cpp
@@ -1,26 +1,31 @@
-//
+
 // Created by Pablo Alcolea Sesse on 2/11/24.
 //
-#include<iostream>
-#include<stdexcept>
+#include <iostream>
+#include <stdexcept>
 
+// Función que lanza una excepción de tipo runtime_error
 void func3() {
     throw std::runtime_error("Error en func3");
 }
 
+// Función que llama a func3
 void func2() {
     func3();
 }
 
+// Función que llama a func2
 void func1() {
     func2();
 }
 
 int main() {
     try {
+        // Llama a func1, lo que eventualmente lanza una excepción
         func1();
     }
     catch (const std::runtime_error& e) {
+        // Captura la excepción lanzada y muestra un mensaje de error
         std::cout << "Excepción capturada en main: " << e.what() << std::endl;
     }
 
