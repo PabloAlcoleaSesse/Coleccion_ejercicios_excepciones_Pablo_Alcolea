fc4c11eaf1e18efb642db63e038a21e1a89a197d
Pablo Alcolea Sesse <128633352+PabloAlcoleaSesse@users.noreply.github.com>
2024-11-04 10:55:53 +0100

    Añadir comentarios al ejercio 7

--- Ej_7/main.cpp
+++ Ej_7/main.cpp
@@ -1,25 +1,32 @@
 //
 // Created by Pablo Alcolea Sesse on 2/11/24.
 //
-#include<iostream>
-#include<stdexcept>
-#include<fstream>
+#include <iostream>
+#include <stdexcept>
+#include <fstream>
 
+// Función que escribe en un archivo
 void escribeEnArchivo(std::ofstream& file) {
     if (!file.is_open()) {
+        // Lanza una excepción si el archivo está cerrado
         throw std::runtime_error("No se puede escribir en un archivo cerrado");
     }
+    // Escribe en el archivo
     file << "Hola, mundo!";
 }
 
 int main() {
+    // Abre un archivo para escritura
     std::ofstream file("archivo.txt");
 
     try {
-        file.close(); // Cierra el archivo para simular un error
+        // Cierra el archivo para simular un error
+        file.close();
+        // Intenta escribir en el archivo cerrado
         escribeEnArchivo(file);
     }
     catch (const std::runtime_error& e) {
+        // Captura la excepción y muestra el mensaje de error
         std::cout << "Excepción capturada: " << e.what() << std::endl;
     }
 
