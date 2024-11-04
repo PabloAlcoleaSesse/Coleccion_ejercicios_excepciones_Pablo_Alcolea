a942895fbbd5dc8e42d092607808717a0ab52526
Pablo Alcolea Sesse <128633352+PabloAlcoleaSesse@users.noreply.github.com>
2024-11-04 10:50:20 +0100

    Añadir comentarios y mejorar el ej 1

--- Ej_1/main.cpp
+++ Ej_1/main.cpp
@@ -1,20 +1,29 @@
 #include<iostream>
 using namespace std;
+
+
 // Variable global que actúa como bandera de error
 bool errorFlag = false;
 
+// Función que realiza la división y maneja el error de división por cero
 double divide(double a, double b) {
     if (b == 0) {
         // Si ocurre un error, se establece la bandera de error
         errorFlag = true;
-
-    }
-    else {
+    } else {
+        // Si no hay error, se realiza la división
         return a / b;
     }
+    // Retorna 0 en caso de error
     return 0;
 }
 
+
+
+/* Cambios significativos:
+   Cambio de int a double en la función divide para aceptar resultados decimales.
+   Impresión del resultado de la división si no hay error.
+*/
 int main() {
 
 
